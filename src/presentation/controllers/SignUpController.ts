8e9f7140c65778bca4947af028ad0de848b14d96
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-05-24 16:58:06 -0300

    feat: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/SignUpController.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+export class SignUpController {
+  handle(httpRequest: any): any {
+    return {
+      statusCode: 400,
+    }
+  }
+}
