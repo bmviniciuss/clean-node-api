6b4a3d11553731c5a9f878b877788248f4d96fe2
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-05-24 17:09:32 -0300

    feat: ensure SignUpController returns 400 if no name is provided

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 export class SignUpController {
   handle(httpRequest: any): any {
-    return {
-      statusCode: 400,
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name'),
+      }
     }
   }
 }
