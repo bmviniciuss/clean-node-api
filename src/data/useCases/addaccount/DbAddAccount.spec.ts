ead9cbd567c08cc0e5792067383a9bec9a844605
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-06-06 05:32:05 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/useCases/addaccount/DbAddAccount.spec.ts
+++ src/data/useCases/addaccount/DbAddAccount.spec.ts
@@ -109,4 +109,23 @@ describe('DbAddAccount usecase', () => {
 
     await expect(promise).rejects.toThrow()
   })
+
+  it('Should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password',
+    })
+  })
 })
