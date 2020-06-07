8d60e8845af7d6f6c470b9bfe817220793693991
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-06-07 03:08:40 -0300

    feat: ensure cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,9 @@
 import { Express } from 'express'
 
 import { bodyParser } from '../middlewares/bodyParser'
+import { cors } from '../middlewares/cors'
 
 export default function (app: Express): void {
   app.use(bodyParser)
+  app.use(cors)
 }
