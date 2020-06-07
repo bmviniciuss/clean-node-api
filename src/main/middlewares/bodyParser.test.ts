b335641843c9fe8704e8efbb91eaf7c2d826c377
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-06-07 02:53:53 -0300

    feat: ensure requests parse body as json

---
+++ src/main/middlewares/bodyParser.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest'
+
+import app from '../config/app'
+
+describe('Body Parser middleware', () => {
+  it('Should parse body as json', async () => {
+    // Create a Test Route to test middleware integration
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post('/test_body_parser')
+      .send({
+        name: 'Vinicius Barbosa',
+      })
+      .expect({
+        name: 'Vinicius Barbosa',
+      })
+  })
+})
