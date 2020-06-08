1c1616a5c388d943cfd70972e1fc458b4e4de4cb
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-06-08 01:39:35 -0300

    refactor: change makeSignupController file name

--- src/main/routes/signupRoutes.ts
+++ src/main/routes/signupRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 
 import { adaptRoute } from '../adapters/expressRouteAdapter'
-import { makeSignupController } from '../factories/signup'
+import { makeSignupController } from '../factories/makeSignupController'
 
 export default function (router: Router): void {
   router.post('/signup', adaptRoute(makeSignupController()))
