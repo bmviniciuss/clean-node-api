53f0006d42bac1ec55ae10d10d6a729fb8317eea
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-06-27 23:15:46 -0300

    feat: ensure SignUpController returns 400 if Validation returns an error

--- src/presentation/controllers/Signup/SignUpController.ts
+++ src/presentation/controllers/Signup/SignUpController.ts
@@ -15,7 +15,10 @@ export class SignUpController implements Controller {
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      this.validation.validate(httpRequest.body)
+      const validationError = this.validation.validate(httpRequest.body)
+      if (validationError) {
+        return badRequest(validationError)
+      }
 
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
