c786915707c742b51c30529c05da6bf135b0cb2b
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-07-04 19:17:56 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/cryptography/jwt/JwtAdapter.spec.ts
+++ src/infra/cryptography/jwt/JwtAdapter.spec.ts
@@ -35,4 +35,11 @@ describe('', () => {
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
+
+  it('Should throw if sign throws', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
+  })
 })
