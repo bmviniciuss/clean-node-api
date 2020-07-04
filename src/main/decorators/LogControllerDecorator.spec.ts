eb2ba904bad75b6569491bcd4542e910ca35d1e2
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-07-04 20:15:38 -0300

    refactor: move all log data protocols to a folder

--- src/main/decorators/LogControllerDecorator.spec.ts
+++ src/main/decorators/LogControllerDecorator.spec.ts
@@ -1,4 +1,4 @@
-import { LogErrorRepository } from '../../data/protocols/db/LogErrorRepository'
+import { LogErrorRepository } from '../../data/protocols/db/log/LogErrorRepository'
 import { AccountModel } from '../../domain/models/Account'
 import { serverError, OK } from '../../presentation/helpers/http'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
--- src/main/decorators/LogControllerDecorator.ts
+++ src/main/decorators/LogControllerDecorator.ts
@@ -1,4 +1,4 @@
-import { LogErrorRepository } from '../../data/protocols/db/LogErrorRepository'
+import { LogErrorRepository } from '../../data/protocols/db/log/LogErrorRepository'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 
 export class LogControllerDecorator implements Controller {
