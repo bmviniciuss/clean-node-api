43257f14b14d309c96a5220c779d6985bab1569f
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-07-04 07:47:35 -0300

    test: ensure DbAuthentication returns a token on success

--- src/data/useCases/authentication/DbAuthentication.spec.ts
+++ src/data/useCases/authentication/DbAuthentication.spec.ts
@@ -125,4 +125,10 @@ describe('DbAuthentication', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  it('Should DbAuthentication returns a token on success', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBe('any_token')
+  })
 })
