9a9fbee542c5df850817cb940cc8e489e190645c
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-07-05 00:03:24 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/__tests__/loginRoutes.test.ts
+++ src/main/routes/__tests__/loginRoutes.test.ts
@@ -52,5 +52,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    it('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'vinicius_barbosa@gmail.com',
+          password: '1234'
+        })
+        .expect(401)
+    })
   })
 })
