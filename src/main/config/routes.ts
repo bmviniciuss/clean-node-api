01f3b7ac9e2ab511463bcb64673bc8b5416f4b9a
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2020-12-26 01:58:19 -0300

    fix: use path.join on routes config

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,11 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
+import path from 'path'
 
 export default function (app: Express): void {
   const router = Router()
   app.use('/api', router)
-  readdirSync(`${__dirname}/../routes`).map(async (file) => {
+  readdirSync(path.join(__dirname, '..', 'routes')).map(async (file) => {
     if (!(file.includes('.test.') || file.includes('__tests__'))) {
       (await import(`../routes/${file}`)).default(router)
     }
